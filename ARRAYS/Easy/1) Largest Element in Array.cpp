15cf2eee1b0bd0449a735c23a197e7a4cdafa9b1
Lokesh Dilip Bandurkar <lokeshdan770@gmail.com>
2025-09-01 21:12:51 +0530

    Update 1) Largest Element in Array.cpp

--- ARRAYS/Easy/1) Largest Element in Array.cpp
+++ ARRAYS/Easy/1) Largest Element in Array.cpp
@@ -1,7 +1,9 @@
 class Solution {
   public:
     int largest(vector<int> &arr) {
+        // considering first element as largest
         int large = arr[0];
+        // comparing with remaining elements if there exists a largest
         for(int i = 1; i < arr.size(); i++) {
             if(arr[i] > large) {
                 large = arr[i];
