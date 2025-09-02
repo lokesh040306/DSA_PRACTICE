a4b539e77892bfb1d32d5bd1e47949bde3734662
Lokesh Dilip Bandurkar <lokeshdan770@gmail.com>
2025-09-02 20:02:40 +0530

    Create 4) 26. Remove Duplicates from Sorted Array.cpp

---
+++ ARRAYS/Easy/4) 26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // TWO-POINTERS
+        int left = 0, right = 0;
+        // int index = 0;
+        int n = nums.size();
+
+        while(right < n) {
+            if(nums[left] != nums[right]) {
+                nums[left + 1] = nums[right];
+                left++;
+            }
+            right++;
+        }
+        return left + 1;
+    }
+};
